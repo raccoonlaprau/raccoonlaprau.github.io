77d7e05504c30bf1728e0374c7f392a3796aede3
Laprau <34046795+raccoonlaprau@users.noreply.github.com>
2023-07-24 20:34:38 -0500

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center p-24">
+      <h1>Under Construction!</h1>
+      <p>I only just revived this site! Give me some time!</p>
+    </main>
+  )
+}
