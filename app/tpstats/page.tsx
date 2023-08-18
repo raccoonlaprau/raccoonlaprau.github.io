bb9fffd213a887715060454d16879f106b852f80
Laprau <34046795+raccoonlaprau@users.noreply.github.com>
2023-08-17 20:05:28 -0500

    add tp stats page

---
+++ app/tpstats/page.tsx
@@ -0,0 +1,42 @@
+'use client'
+import { useEffect, useState } from "react";
+import { supabase } from "../utils/supabase"
+
+export default function TrashPandaStats() {
+  const [soundCounts, setSoundCounts] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchSounds = async () => {
+    try {
+     setLoading(true);
+     const { data, error } = await supabase
+       .from("tp-sounds")
+       .select("*")
+
+     if (error) throw error;
+     setSoundCounts(data);
+   } catch (error : any) {
+     alert(error.message);
+   } finally {
+     setLoading(false);
+   }
+}
+
+  useEffect(() => {
+    fetchSounds();
+  }, []);
+
+  if (loading) {
+    return <div>Fetching data...</div>;
+  }
+
+  return (
+    <div>
+      { soundCounts?.map( (soundData) => (
+        <p>{soundData.sound_name} played {soundData.times_played} times.</p>
+      ))}
+    </div>
+  )
+}
+
+
